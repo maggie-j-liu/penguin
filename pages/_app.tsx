1445c82a2dc06f8a4fe908f6d07487ee10168b91
maggie-j-liu <maggie.j.liu@gmail.com>
2022-10-22 11:25:05 -0700

    login working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { session, ...otherProps } = pageProps as any;
+  return (
+    <SessionProvider session={session}>
+      <Component {...otherProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
