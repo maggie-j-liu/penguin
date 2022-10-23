ded7b1b9c4dbbb6e2722c2cccf971f67e343a5e6
maggie-j-liu <maggie.j.liu@gmail.com>
2022-10-22 19:20:21 -0700

    new waiver states

--- pages/waiver/[participantWaiver].tsx
+++ pages/waiver/[participantWaiver].tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from "next";
 import { useState } from "react";
 import PageLayout from "../../components/PageLayout";
 import prisma from "../../lib/prisma";
+import { useRouter } from "next/router";
 
 const WaiverPage = ({
   participant: { event, ...participant },
@@ -10,19 +11,15 @@ const WaiverPage = ({
   participant: Participant & { event: Event };
 }) => {
   const [uploadingImage, setUploadingImage] = useState(false);
-  const [done, setDone] = useState(false);
-
-  if (
-    participant.waiverStatus === WaiverStatus.APPROVED ||
-    participant.waiverStatus === WaiverStatus.PENDING
-  ) {
+  const router = useRouter();
+  if (participant.waiverStatus === WaiverStatus.APPROVED) {
     return (
       <div className="space-y-6 py-12 px-6 text-center">
         <div className="space-y-1 text-center">
           <h1 className="text-center text-4xl font-black">
             Waiver for {event.name} already signed.
           </h1>
-          <p>
+          <p className="text-xl">
             {participant.waiverStatus === WaiverStatus.APPROVED
               ? "The organizers have reviewed your waiver submission and have approved it!"
               : "Waiver is pending approval from the organizers."}
@@ -43,73 +40,97 @@ const WaiverPage = ({
     );
   }
 
-  if (!done) {
-    return (
-      <PageLayout noNavbar>
-        <h1 className="text-center text-5xl font-bold">
-          Sign {event.name} Waiver
-        </h1>
-        <p className="mt-2 text-center text-xl">
-          Print, sign, and scan the waiver at{" "}
-          <a className="underline" href={event.waiverLink}>
-            {event.waiverLink}
-          </a>
-        </p>
-        <input
-          name="image"
-          type="file"
-          className="mx-auto block w-full"
-          // accept="image/*, video*/"
-          accept="image/png, image/jpeg, image/jpg"
-          multiple
-          onChange={async (e) => {
-            if (e.target.files) {
-              setUploadingImage(true);
-              const fd = new FormData();
-              Array.from(e.target.files).forEach((file, i) => {
-                fd.append(file.name, file);
-              });
-              fd.append("waiverId", participant.waiverId);
-              const media = await fetch("/api/waiver/upload", {
-                method: "POST",
-                body: fd,
-              });
-              const newFiles = await media.json();
-              // setFiles((f) => [...f, ...newFiles]);
-              e.target.value = "";
-              setUploadingImage(false);
-              setDone(true);
-              // console.log(e.target.files);
-            }
-          }}
-        />
-        {uploadingImage ? (
-          <p className="dark:text-gray-300">Uploading image(s)...</p>
-        ) : null}
-      </PageLayout>
-    );
-  }
   return (
-    <div className="space-y-2 py-12 text-center">
-      <h1 className="text-center text-4xl font-black">
-        Waiver submitted for {event.name}!
+    <PageLayout noNavbar>
+      <h1 className="text-center text-5xl font-bold">
+        Sign {event.name} Waiver
       </h1>
-      <p>
-        You will receive updates on your email regarding the status of your
-        waiver. You can now close this page.
+      <p className="mx-auto mt-2 max-w-4xl text-center text-xl">
+        {participant.waiverStatus === WaiverStatus.PENDING ? (
+          <>
+            Your waiver is pending approval from the organizers. If you want to
+            resubmit, you can do so below. You can find the waiver at{" "}
+            <a className="underline" href={event.waiverLink}>
+              {event.waiverLink}
+            </a>
+          </>
+        ) : participant.waiverImages.length !== 0 ? (
+          <>
+            Your waiver was rejected by the organizers. Please fill out the
+            waiver and resubmit it. You can find the waiver at{" "}
+            <a className="underline" href={event.waiverLink}>
+              {event.waiverLink}
+            </a>
+          </>
+        ) : (
+          <>
+            Print out the waiver at{" "}
+            <a className="underline" href={event.waiverLink}>
+              {event.waiverLink}
+            </a>
+            . Fill it out and upload the images below.
+          </>
+        )}
       </p>
-
-      <div className="flex justify-center">
+      <div className="mt-4 flex flex-col items-center">
         <div>
-          <p className="text-left font-semibold">Uploaded file(s):</p>
-          <div className="flex max-w-2xl space-x-3 overflow-x-scroll">
-            {participant.waiverImages.map((waiver) => {
-              return <img key={waiver} src={waiver} className="max-w-md" />;
-            })}
-          </div>
+          <input
+            name="image"
+            type="file"
+            className="mx-auto block w-full"
+            // accept="image/*, video*/"
+            accept="image/png, image/jpeg, image/jpg"
+            multiple
+            onChange={async (e) => {
+              if (e.target.files) {
+                setUploadingImage(true);
+                const fd = new FormData();
+                Array.from(e.target.files).forEach((file, i) => {
+                  fd.append(file.name, file);
+                });
+                fd.append("waiverId", participant.waiverId);
+                const media = await fetch("/api/waiver/upload", {
+                  method: "POST",
+                  body: fd,
+                });
+                const newFiles = await media.json();
+                // setFiles((f) => [...f, ...newFiles]);
+                e.target.value = "";
+                setUploadingImage(false);
+                router.replace(router.asPath);
+
+                // console.log(e.target.files);
+              }
+            }}
+          />
+          {uploadingImage ? (
+            <p className="dark:text-gray-400">Uploading image(s)...</p>
+          ) : null}
         </div>
       </div>
-    </div>
+      {participant.waiverImages.length !== 0 ? (
+        <div className="space-y-2 py-12 text-center">
+          <h2 className="text-center text-4xl font-black">
+            Current waiver [
+            {participant.waiverStatus === WaiverStatus.PENDING
+              ? "⌛ PENDING"
+              : "❌ REJECTED"}
+            ]
+          </h2>
+
+          <div className="flex justify-center">
+            <div>
+              <p className="text-left font-semibold">Uploaded file(s):</p>
+              <div className="flex max-w-2xl space-x-3 overflow-x-scroll">
+                {participant.waiverImages.map((waiver) => {
+                  return <img key={waiver} src={waiver} className="max-w-md" />;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </PageLayout>
   );
 };
 
